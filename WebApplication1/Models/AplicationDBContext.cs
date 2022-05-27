3a3f964150930beb8acfe0f9e691a89e4b029336
Marcelo <marartb4@hotmail.com>
2022-05-27 02:55:37 -0300

    Add project files.

---
+++ WebApplication1/Models/AplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class AplicationDBContext :DbContext
+    {
+
+        public AplicationDBContext(DbContextOptions<AplicationDBContext>
+            options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<TarjetaCredito> tarjetacredito { get; set; }
+
+    }
+}
---
+++ WebApplication1/Models/TarjetaCredito.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class TarjetaCredito
+    {
+
+        [Key]
+        
+        public int id { get; set; }
+        public string Titular { get; set; }
+        public string NumeroTarjeta { get; set; }
+        public string FechaExpiracion { get; set; }
+        public string cvv { get; set; }
+    }
+}
